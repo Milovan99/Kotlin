3469d24afec29d30b9ccc87919abe592fc2f8930
Milovan99 <milovanjakovljevic78@gmail.com>
2022-12-11 14:52:27 +0100

    Eksplicitna deklaracija

---
+++ src/main/kotlin/Tipovi_podataka/Eksplicitna_deklaracija.kt
@@ -0,0 +1,15 @@
+package Tipovi_podataka
+
+class TypeAnotation{
+    //eksplicitno definisanje tipa promenjive
+
+    val ime : String = ""
+    val godine : Int = 23
+    val prosek :Double=8.23
+    val prosekLong :Long=8
+    val prosekShort:Short = 8
+    val prosekByte : Byte = 8
+    val prosekFloat : Float = 8.23f
+    val diplomirao : Boolean = true
+
+}
